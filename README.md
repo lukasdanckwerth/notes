69c8bedda77b2b27d822ecfc6c3e3ce3428244b0
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2021-10-31 12:41:59 +0100

    update install-apache2.sh

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 ### Development
 ```bash
-# create multipass ubuntu instace
+# create multipass ubuntu instance
 $ multipass launch -n development-server -d 7G -c 2 -m 2G
+
+# connect to new installed instance
+$ multipass shell development-server
+
+# long url
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/lukasdanckwerth/install-apache2-server/main/install-apache2.sh)"
+
 ```
---
+++ install-apache2.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+set -x
+
+log() {
+  echo "[install-apache2.sh] ${*}";
+}
+
+# ========================================
+# CONSTANTS
+IA_SERVERNAME_FILE_PATH="/etc/apache2/conf-available/servername.conf"
+IA_HOSTNAME=$(hostname)
+log 'hostname: ' "${IA_HOSTNAME}"
+
+# ========================================
+# UPDATE
+echo "update packages"
+sudo apt update -y
+
+# ========================================
+# INSTALL
+echo "install packages"
+sudo apt install vim apache2 php postgresql postgresql-contrib -y
+
+exit 0
+
+# ========================================
+# CONFIGURE APACHE
+
+# ========================================
+# CONFIGURE POSTGRESQL
+
+ServerName __YOUR_WEB_SITE__
+
+# enable rewrite
+sudo a2enmod rewrite
+
+
+
+$ sudo systemctl is-active postgresql
+$ sudo systemctl is-enabled postgresql
+$ sudo systemctl status postgresql
+$ sudo pg_isready
+
+$ sudo systemctl restart postgresql
+
+$ curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+
+$ sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+
+$ sudo apt install pgadmin4 -y
+
+$ sudo /usr/pgadmin4/bin/setup-web.sh
+$ sudo systemctl restart apache2
+
+$ sudo apt-get install php-pgsql
+
+# ========================================
+# install composer
+sudo apt update && sudo apt install wget php-cli php-zip unzip curl
+cd /tmp/
+curl -sS https://getcomposer.org/installer |php
+sudo mv composer.phar /usr/local/bin/composer
