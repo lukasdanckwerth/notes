c8f395dc0e98b5785149a9af956901a9a14a4bb9
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2021-10-31 19:10:09 +0100

    update

--- install-apache2.sh
+++ install-apache2.sh
@@ -5,6 +5,7 @@ set -e
 IA_SERVERNAME_FILE_PATH="/etc/apache2/conf-available/servername.conf"
 IA_HOSTNAME=$(hostname)
 IA_APACHE_CONFIG="/etc/apache2/apache2.conf"
+IA_DEFAULT_CONFIG_URL="${IS_REPOSITORY_URL}/apache2/apache.conf"
 
 log() {
   echo "[install-apache2]  ${*}"
@@ -24,7 +25,7 @@ log "enable rewrite"
 sudo a2enmod rewrite
 
 echo
-read -r -p "Do you want to replace the config $(bold "${IS_SAMBA_CONFIG}") with the default one from this script? The default config can't viewed at ${IS_DEFAULT_CONFIG_URL}. (y/n) " replaceConfig
+read -r -p "Do you want to replace the config $(bold "${IA_APACHE_CONFIG}") with the default one from this script? The default config can't viewed at ${IA_DEFAULT_CONFIG_URL}. (y/n) " replaceConfig
 
 if [[ "${replaceConfig}" == "y" ]]; then
   echo 'replaceConfig'
