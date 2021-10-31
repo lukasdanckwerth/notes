30065b2d06cf6578b34ae6ee5d0f55831b77c3aa
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2021-10-31 21:32:23 +0100

    version 30

--- install-server.sh
+++ install-server.sh
@@ -3,7 +3,7 @@ set -u
 set -e
 
 # next line will be replaced by `update-version` command
-INS_VERSION=29
+INS_VERSION=30
 
 export INS_NAME="install-server"
 export INS_REPOSITORY_URL="https://raw.githubusercontent.com/lukasdanckwerth/install-apache2-server/main"
