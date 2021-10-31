fb80b15629f64efc0ee3631c29bd843f823d5007
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2021-10-31 19:02:33 +0100

    update

--- install-apache2.sh
+++ install-apache2.sh
@@ -2,12 +2,13 @@
 set -u
 set -e
 
+IA_SERVERNAME_FILE_PATH="/etc/apache2/conf-available/servername.conf"
+IA_HOSTNAME=$(hostname)
+IA_APACHE_CONFIG="/etc/apache2/apache2.conf"
+
 log() {
   echo "[install-apache2]  ${*}"
 }
-
-IA_SERVERNAME_FILE_PATH="/etc/apache2/conf-available/servername.conf"
-IA_HOSTNAME=$(hostname)
 
 log "install packages"
 sudo apt install -y vim apache2 php
