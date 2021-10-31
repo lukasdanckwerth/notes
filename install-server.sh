2645e97edd99ce2536482deeb63960b03fadd11c
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2021-10-31 20:59:49 +0100

    version 13

--- install-server.sh
+++ install-server.sh
@@ -3,11 +3,7 @@ set -u
 set -e
 
 # next line will be replaced by `update-version` command
-INS_VERSION=1
-
-INS_VERSION=1
-
-INS_VERSION=1
+INS_VERSION=13
 
 export INS_NAME="install-server-automation"
 export INS_REPOSITORY_URL="https://raw.githubusercontent.com/lukasdanckwerth/install-apache2-server/main"
