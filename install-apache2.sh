c97c027092e28ad0c081fa361de45f267663d418
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2021-10-31 19:11:19 +0100

    update

--- install-apache2.sh
+++ install-apache2.sh
@@ -6,6 +6,8 @@ IA_SERVERNAME_FILE_PATH="/etc/apache2/conf-available/servername.conf"
 IA_HOSTNAME=$(hostname)
 IA_APACHE_CONFIG="/etc/apache2/apache2.conf"
 IA_DEFAULT_CONFIG_URL="${IS_REPOSITORY_URL}/apache2/apache.conf"
+IA_INDEX="/var/www/html/index.html"
+IA_INDEX_URL="${IS_REPOSITORY_URL}/apache2/index.html"
 
 log() {
   echo "[install-apache2]  ${*}"
@@ -23,6 +25,13 @@ fi
 
 log "enable rewrite"
 sudo a2enmod rewrite
+
+echo
+read -r -p "Do you want to replace the config $(bold "${IA_APACHE_CONFIG}") with the default one from this script? The default config can't viewed at ${IA_DEFAULT_CONFIG_URL}. (y/n) " replaceConfig
+
+if [[ "${replaceConfig}" == "y" ]]; then
+  echo 'replaceConfig'
+fi
 
 echo
 read -r -p "Do you want to replace the config $(bold "${IA_APACHE_CONFIG}") with the default one from this script? The default config can't viewed at ${IA_DEFAULT_CONFIG_URL}. (y/n) " replaceConfig
