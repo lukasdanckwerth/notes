db7222a3eb36d16d6f909ce33be157f9e022ff4e
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2021-12-13 14:05:12 +0100

    version 98

--- scripts/install-server.sh
+++ scripts/install-server.sh
@@ -3,7 +3,7 @@ set -u
 set -e
 
 # next line will be replaced by `update-version` command
-INS_VERSION=97
+INS_VERSION=98
 
 export INS_NAME="install-server"
 export INS_SEPARATOR="--------------------------------------"
