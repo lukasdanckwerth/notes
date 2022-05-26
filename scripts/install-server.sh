9d57c11972d2acc9581771fbd3f1daccda2602d5
Lukas Danckwerth <lukas.danckwerth@gmx.de>
2022-05-26 19:02:19 +0200

    version 115

--- scripts/install-server.sh
+++ scripts/install-server.sh
@@ -3,7 +3,7 @@ set -u
 set -e
 
 # next line will be replaced by `update-version` command
-INS_VERSION=114
+INS_VERSION=115
 
 export INS_NAME="install-server"
 export INS_SEPARATOR="--------------------------------------"
